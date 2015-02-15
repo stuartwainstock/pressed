02e5b8e67f5aba35225ef4439bb10747a323f986
Stuart Wainstock <stuart.wainstock@gmail.com>
2015-02-15 12:51:36 -0600

    add pressed

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pressed and Squeezed</title>
+	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
+	<link rel="stylesheet" href="http://localhost/wk2/wordpress/wp-content/themes/pressed/style.css"/>
+</head>
+	<body>
+		<header>
+			<h1>Pressed and Squeezed</h1>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>WordPress</li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<img src="http://localhost/wk2/wordpress/wp-content/themes/pressed/images/hero.jpg" />
+			<section>
+				<p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
+				<p>We also make pancakes on the side.</p>
+			</section>
+		</section>
+		<footer>
+			<h1>Contact Us</h1>
+			<p>Pressed and Squeezed</p>
+			<p>2525 Orange Way, Orchard, CA 91104</p>
+			<p>Phone: (800) 424-3232</p>
+		</footer>
+	</body>
+</html>
