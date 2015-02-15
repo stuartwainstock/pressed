fec328b585e68236318a3520e7f8d6c68b09ad40
Stuart Wainstock <stuart.wainstock@gmail.com>
2015-02-15 14:01:53 -0600

    added sidebar

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
-<?php register_nav_menus(); ?>
+<?php 
+
+register_nav_menus(); 
+register_sidebar(array(
+		'before_widget' => '<div class="sidebar">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<div class="title">',
+		'after_title'   => '</div>'
+	));
+
+
+
+?>
